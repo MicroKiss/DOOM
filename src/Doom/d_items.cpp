b5d4131eac56b394432fdf8a8e4412fe23d0cb8b
MicroKiss <dinnyetok@gmail.com>
2026-08-29 12:39:07 +0200

    removed rcsid

--- src/Doom/d_items.cpp
+++ src/Doom/d_items.cpp
@@ -20,9 +20,6 @@
 //
 //-----------------------------------------------------------------------------
 
-static const char
-	rcsid[] = "$Id:$";
-
 // We are referring to sprite numbers.
 extern "C"
 {
